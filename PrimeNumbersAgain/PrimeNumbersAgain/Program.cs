f81574eccf07699fa3cfe37b834f3e5402b2d7a8
NonOxC <59771975+NonOxC@users.noreply.github.com>
2024-11-06 10:55:00 -0500

    Update Program.cs

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -25,7 +25,26 @@ namespace PrimeNumbersAgain
 
         static int FindNthPrime(int n)
         {
-            return 0;
+            int limit = (int)(n * Math.Log(n) * 2);
+            bool[] isPrime = new bool[limit + 1];
+            for (int i = 2; i <= limit; i++) isPrime[i] = true;
+
+            for (int i = 2; i * i <= limit; i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j <= limit; j += i)
+                        isPrime[j] = false;
+                }
+            }
+
+            List<int> primes = new List<int>();
+            for (int i = 2; i <= limit && primes.Count < n; i++)
+            {
+                if (isPrime[i]) primes.Add(i);
+            }
+
+            return primes[n - 1];
         }
 
         static int GetNumber()
